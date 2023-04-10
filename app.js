8be67a173cff3879a157bebe9b50d63059da3d7d
NicolleDanielaSarmientoSerranoADSO <daniisarmientose26@gmail.com>
2023-04-10 16:11:43 -0500

    feat: :rocket: Inicio
    Configuración de modulos

---
+++ app.js
@@ -0,0 +1,4 @@
+import {createapp} from 'https://unpkg.com/browse/vue@3.2.47/dist/vue.esm-browser.prod.js'
+import myModulo from './component/my-modulo'
+
+createapp(myModulo).mount(`#app`)
---
+++ component/my-modulo.js
@@ -0,0 +1,5 @@
+export default{
+    name:my-modulo,
+    template: /*html*/`
+    `,
+}
