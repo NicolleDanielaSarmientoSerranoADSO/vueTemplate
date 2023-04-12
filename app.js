fd844c2530a36272dcab07c18511435c0a4f1bcc
NicolleDanielaSarmientoSerranoADSO <daniisarmientose26@gmail.com>
2023-04-12 10:17:34 -0500

    feat: :sparkles: Implementación de vue
    Adición de vue

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
-import {createapp} from 'https://unpkg.com/browse/vue@3.2.47/dist/vue.esm-browser.prod.js'
-import myModulo from './component/my-modulo'
+import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
+import myModulo from './component/my-modulo.js';
+import myModulo1 from './component/my-modulo1.js';
+import main from './main.js';
 
-createapp(myModulo).mount(`#app`)
+let app = createApp(main);
+
+app.component(myModulo.name, myModulo);
+app.component(myModulo1.name, myModulo1);
+
+app.mount('#app')
